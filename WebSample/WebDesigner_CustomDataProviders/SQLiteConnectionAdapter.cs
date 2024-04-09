c79624bb4fe4cd893e7cde25d31f0d3a01f788b9
yasuhiroo <yasuhiro.okuyama@mescius.com>
2024-04-09 16:00:16 +0900

    Add files via upload
    add WebDesigner_Custom, WebDesigner_CustomDataProviders, WebDesigner_CustomSharedDataSources

---
+++ WebSample/WebDesigner_CustomDataProviders/SQLiteConnectionAdapter.cs
@@ -0,0 +1,20 @@
+﻿using GrapeCity.ActiveReports.Rendering.Data;
+using System;
+using System.Globalization;
+using System.Linq;
+
+namespace WebDesignerCustomDataProviders
+{
+	public sealed class SQLiteConnectionAdapter : DbConnectionAdapter
+	{
+		public static SQLiteConnectionAdapter Instance = new SQLiteConnectionAdapter();
+
+		/// <summary>
+		/// Returns the string representation of a multi-value parameter's value.
+		/// </summary>
+		protected override string MultivalueParameterValueToString(object[] parameterArrayValue)
+		{
+			return string.Join(",", parameterArrayValue.Select(parameterValue => "'" + Convert.ToString(parameterValue, CultureInfo.InvariantCulture) + "'"));
+		}
+	}
+}
