19c5701d00833d789c52187fbdbe5b3b2206d8d4
yasuhiroo <yasuhiro.okuyama@mescius.com>
2024-04-09 17:11:30 +0900

    Add files via upload
    add EndUserDesigner

---
+++ DesktopSample/DesignerPro/EndUserDesigner/C#/EndUserDesigner/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EndUserDesigner")]
+[assembly: AssemblyProduct("EndUserDesigner")]
+[assembly: AssemblyCopyright("Copyright(c) 2003 MESCIUS inc.")]
+
+//次の GUID は、このプロジェクトが COM に公開される場合の、typelib の ID です。
+[assembly:Guid("E550208B-1010-4F51-A488-2E05F64E3AD1")]
+
+
+//アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+
+//	 Major Version
+//	 Minor Version 
+//	 Build Number
+//	 Revision
+//
+//すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
+//既定値にすることができます:
+
+
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ DesktopSample/DesignerPro/EndUserDesigner/VB.NET/EndUserDesigner/My Project/AssemblyInfo.vb
@@ -0,0 +1,28 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+
+'アセンブリに関する一般情報は以下の
+'属性セットを通して制御されます。アセンブリに関連付けられている
+'情報を変更するには、これらの属性値を変更してください。
+
+<Assembly: AssemblyTitle("EndUserDesigner")>
+<Assembly: AssemblyProduct("EndUserDesigner")> 
+<Assembly: AssemblyCopyright("Copyright(c) 2003 MESCIUS inc.")> 
+'次の GUID は、このプロジェクトが COM に公開される場合の、typelib の ID です。
+<Assembly: Guid("CB03D058-4BFB-41C2-B21D-A836A4291476")> 
+
+
+'アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+
+'	 Major Version
+'	 Minor Version 
+'	 Build Number
+'	 Revision
+
+'すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
+'既定値にすることができます:
+
+
+<Assembly: AssemblyVersion("1.0.0.0")>
