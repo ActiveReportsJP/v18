5019209c89a6915814eafe59ea93ef25b4b7f0f2
yasuhiroo <yasuhiro.okuyama@mescius.com>
2024-04-09 17:14:06 +0900

    Add files via upload
    add Map, TableOfContents

---
+++ DesktopSample/DesignerPro/Map/VB.NET/Map Gallery/My Project/AssemblyInfo.vb
@@ -0,0 +1,13 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+'アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+'アセンブリに関連付けられている情報を変更するには、。
+'これらの属性値を変更してください。
+
+<Assembly: AssemblyTitleAttribute("MapGallery")>
+<Assembly: AssemblyProductAttribute("MapGallery")> 
+<Assembly: AssemblyCopyrightAttribute("Copyright(c) 2003 MESCIUS inc.")> 
+<Assembly: AssemblyVersionAttribute("1.0.0.0")> 
+<Assembly: AssemblyFileVersionAttribute("1.0.0.0")>
